28eec4ecc6328e2735fc297b96026fab1c18cdfb
ashin <ashin_555@outlook.com>
2025-09-28 14:52:35 +0800

    fix(background): update random wallpaper api url
    将背景图片的随机 API 地址从 "https://api.sqmn.eu.org/img/random" 更改为 "https://api.sqmn.eu.org/wallpaper/random"，以确保正确获取壁纸资源。

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -39,7 +39,7 @@ const bgRandom = Math.floor(Math.random() * 10 + 1);
 // 更换壁纸链接
 const changeBg = (type) => {
   if (type == 0) {
-    bgUrl.value = "https://api.sqmn.eu.org/img/random";
+    bgUrl.value = "https://api.sqmn.eu.org/wallpaper/random";
   } else if (type == 1) {
     bgUrl.value = "https://api.dujin.org/bing/1920.php";
   } else if (type == 2) {
