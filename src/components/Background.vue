86f352f7b94d811d52eef3ade5a33e6f05fff526
ashin <ashin_555@outlook.com>
2025-09-29 00:36:18 +0800

    refactor(config): 更新站点名称和背景音乐API地址 将站点名称从"无悔の主页"简化为"无悔"，并更换壁纸API地址以使用新的随机背景服务

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -39,7 +39,7 @@ const bgRandom = Math.floor(Math.random() * 10 + 1);
 // 更换壁纸链接
 const changeBg = (type) => {
   if (type == 0) {
-    bgUrl.value = "https://api.sqmn.eu.org/wallpaper/random";
+    bgUrl.value = "https://random.sqmn.eu.org";
   }
 };
 
