6e24a74b9d1c8cd652ac4900d2371ed3d57f18f9
ashin <ashin_555@outlook.com>
2025-09-28 22:30:38 +0800

    refactor(ui): 更新站点链接配置并引入邮箱快捷方式 移除多个站点链接配置项，包括 FreshRss、RssHub、音乐解锁、网盘、IP检测和 Github 代理。 保留并新增邮箱（mail.sqmn.eu.org）链接，同时在 Links.vue 组件中引入对应图标。

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, RssSquare, Music, Toolbox, Github, Rss, Database} from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, RssSquare, Music, Toolbox, Github, Rss, Database, Envelope} from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -76,6 +76,7 @@ const siteIcon = {
   Github,
   Rss,
   Database,
+  Envelope,
 };
 
 // 链接跳转
