ea272e5e2a2d153bb2d5b52e98b892e8f8d21ce7
ashin <ashin_555@outlook.com>
2025-09-29 16:07:36 +0800

    fix(ui): 解决背景图片缓存问题，添加时间戳参数以确保更新生效

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -39,7 +39,7 @@ const bgRandom = Math.floor(Math.random() * 10 + 1);
 // 更换壁纸链接
 const changeBg = (type) => {
   if (type == 0) {
-    bgUrl.value = "https://random.sqmn.eu.org";
+    bgUrl.value = "https://random.sqmn.eu.org" + "?t=" + new Date().getTime();
   }
 };
 
